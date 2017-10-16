fbe7dd5fcdf2b9e6a5d33e9af262abdc494d9bae
javion25 <228806684@qq.com>
2017-10-16 14:34:38 +0800

    update

--- index.ios.js
+++ index.ios.js
@@ -17,7 +17,7 @@ export default class MiaowApp extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to 你好!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
